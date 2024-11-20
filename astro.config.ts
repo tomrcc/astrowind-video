a3d93a203aabb30a0d08d0735b8413dcdac94480
tomrcc <tom@cloudcannon.com>
2024-11-21 10:23:12 +1300

    Add bookshop

--- astro.config.ts
+++ astro.config.ts
@@ -10,6 +10,7 @@ import partytown from '@astrojs/partytown';
 import icon from 'astro-icon';
 import compress from 'astro-compress';
 import type { AstroIntegration } from 'astro';
+import bookshop from '@bookshop/astro-bookshop';
 
 import astrowind from './vendor/integration';
 
@@ -29,6 +30,7 @@ export default defineConfig({
       applyBaseStyles: false,
     }),
     sitemap(),
+    bookshop(),
     mdx(),
     icon({
       include: {
